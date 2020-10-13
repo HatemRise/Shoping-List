bf5887cf3c8b0b7e9b6071fab8355c05b28ea1d3
Resolator666 <wdawdx@yandex.ru>
2020-10-13 23:57:55 +0300

    вроде как доделал настройки. надеюсь, что хоть что-то там работает

--- src/entites/settings.java
+++ src/entites/settings.java
@@ -1,4 +1,48 @@
 package entites;
+import java.io.*;
 
-public class settings {
+public class settings implements Serializable {
+    User user = null;
+    String server_id = null;
+    int update_interval = 0;
+    boolean autoupdate = true;
+
+    public settings (User user, String server_id, int update_interval, boolean autoupdate){
+        this.user = user;
+        this.server_id = server_id;
+        this.update_interval = update_interval;
+        this.autoupdate = autoupdate;
+    }
+    public User getUser(){
+        return user;
+    }
+    public void setUser(User value){
+        user = value;
+    }
+    public String getServer_id(){
+        return server_id;
+    }
+    public void setServer_id(String value){
+        server_id = value;
+    }
+    public int getUpdate_interval(){
+        return update_interval;
+    }
+    public void setUpdate_interval(int value){
+        update_interval = value;
+    }
+    public boolean getAutoupdate(){
+        return autoupdate;
+    }
+    public void setAutoupdate(boolean value) {
+        autoupdate = value;
+    }
+    static void serialize(settings empObj) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream("data.obj");
+             ObjectOutputStream oos = new ObjectOutputStream(fos))
+        {
+            oos.writeObject(empObj);
+        }
+
+    }
 }
